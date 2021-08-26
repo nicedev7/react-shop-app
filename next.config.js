e60dff1183ba302f4e6f2da8d0a5be0e7892c75e
krismanit <jackiestar7@outlook.com>
2021-08-26 09:29:13 +0800

    chore: change next build config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withPlugins = require('next-compose-plugins')
 const withSvg = require('./next-plugins/next-svg')
 
 module.exports = withPlugins([withSvg], {
-  distDir: 'build',
   poweredByHeader: false,
   webpack5: false,
   env: {
